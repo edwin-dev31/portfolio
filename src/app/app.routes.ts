d9048440a0da3d3d02bd11da919734b4721a2e96
edwin-dev31 <edwin_dev@hotmail.com>
2026-03-14 17:31:25 -0500

    feat: configure app routes for public and admin sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/public/components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'project/:id',
+    loadComponent: () => import('./features/public/components/project-detail/project-detail.component').then(m => m.ProjectDetailComponent)
+  }
+];
